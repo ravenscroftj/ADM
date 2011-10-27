c3a412003636f45e4a38ca9dcf48a686ba0082a8
James Ravenscroft <ravenscroftj@gmail.com>
2011-10-27 21:23:50 +0100

    We now have a functional sexy link checker.

--- src/com/funkymonkeysoftware/adm/LinkCheckerActivity.java
+++ src/com/funkymonkeysoftware/adm/LinkCheckerActivity.java
@@ -1,10 +1,15 @@
 package com.funkymonkeysoftware.adm;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.funkymonkeysoftware.adm.download.HTTPChecker;
 import com.funkymonkeysoftware.adm.download.LinkChecker;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -20,37 +25,32 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+/**
+ * User interface class for the link checker activity
+ * 
+ * @author James Ravenscroft
+ *
+ */
 public class LinkCheckerActivity extends Activity implements OnClickListener{
 	
+	/**
+	 * This is a reference to the add links button
+	 */
 	private Button addLinksBtn;
-	private DownloadsDBOpenHelper dbhelper;
 	
 	/**
-	 * This class  runs URLCheckers over all the URLs
-	 * 
-	 * @author James Ravenscroft
-	 *
+	 * Button that forces the link checker to go off and do its stuff
 	 */
-	private class LinkCheckerTask extends AsyncTask<URL, Integer, String[]>{
-
-		@Override
-		protected String[] doInBackground(URL... params) {
-		
-			String[] result = new String[params.length];
-			
-			for(int i=0; i < params.length; i++){
-				//check the status of the named url
-				URL current = params[i];
-				
-				//TODO: integrate checker manager here that runs different checkers
-				LinkChecker chk;
-			}
-			
-			return result;
-		}
-		
-	}
+	private Button checkLinksBtn;
 	
+	/**
+	 * Object designed to assist in the opening and closing of the db
+	 */
+	private DownloadsDBOpenHelper dbhelper;
+	
+	private ProgressDialog pdialog;
+	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -64,14 +64,21 @@ public class LinkCheckerActivity extends Activity implements OnClickListener{
 		addLinksBtn = (Button)findViewById(R.id.addLinksBtn);
 		addLinksBtn.setOnClickListener(this);
 		
+		checkLinksBtn = (Button)findViewById(R.id.checkLinksBtn);
+		checkLinksBtn.setOnClickListener(this);
+		
 		//load the links
 		loadLinks();
 	}
+	
 	
 	private void loadLinks(){
 		//get the table to append things to
 		TableLayout table = (TableLayout)findViewById(R.id.checkLinksTable);
 	
+		//empty the table view
+		table.removeAllViews();
+		
 		//get all downloads that are unchecked or online or offline
 		SQLiteDatabase db = dbhelper.getWritableDatabase();
 		
@@ -122,10 +129,123 @@ public class LinkCheckerActivity extends Activity implements OnClickListener{
 
 	@Override
 	public void onClick(View v) {
+		
 		if(v.equals(addLinksBtn)){
 			//show the add links view
 			startActivity(new Intent(this, LinkInputActivity.class));
+		}else if(v.equals(checkLinksBtn)){
+				checkUrls();
 		}
 	}
 	
+	
+	/**
+	 * This function carries out initialisation of the link checker etc
+	 */
+	private void checkUrls(){
+		
+		//get a read only db
+		SQLiteDatabase db = dbhelper.getReadableDatabase();
+		
+		Cursor c = db.rawQuery("SELECT * FROM downloads WHERE " +
+				"status='unchecked' OR status='online' OR " +
+				"status='offline'", null);
+		
+		//get all urls and store in array
+		URL[] urls = new URL[c.getCount()];
+		
+		for(int i=0; c.moveToNext(); i++){
+			try {
+				urls[i] = new URL(c.getString(1));
+			} catch (MalformedURLException e) {
+				urls[i] = null;
+			}
+		}
+		
+		LinkCheckerTask task = new LinkCheckerTask();
+		
+		pdialog = new ProgressDialog(this);
+		pdialog.setMax(c.getCount());
+		pdialog.setProgress(0);
+		
+		//set progress dialog text
+		pdialog.setMessage(String.format("Checking URL 1/%d", c.getCount()));
+		
+		//show the progress bar
+		pdialog.show();
+		
+		//run the task
+		task.execute(urls);		
+	}
+	
+	
+	/**
+	 * This class  runs URLCheckers over all the URLs
+	 * 
+	 * @author James Ravenscroft
+	 *
+	 */
+	private class LinkCheckerTask extends AsyncTask<URL, Integer, String[]>{
+
+		URL[] theURLS;
+		
+		@Override
+		protected String[] doInBackground(URL... params) {
+		
+			//make a reference to the url array
+			theURLS = params;
+			
+			String[] result = new String[params.length];
+			
+			LinkChecker chk = new HTTPChecker();
+			
+			for(int i=0; i < params.length; i++){
+				//check the status of the named url			
+				try {
+					result[i] = chk.checkURL(params[i]);
+				} catch (IOException e) {
+					//if there was an IO exception, assume it to be offline
+					result[i] = "offline";
+				}
+				
+				publishProgress(i+1);
+			}
+			
+			return result;
+		}
+
+		/**
+		 * When an update to the progress is made, update the shiny!
+		 * 
+		 */
+		protected void onProgressUpdate(Integer... progress){
+				pdialog.incrementProgressBy(1);
+				//set text
+				pdialog.setMessage(String.format("Checking URL %d/%d", 
+						progress[0], theURLS.length));
+		}
+		
+		protected void onPostExecute(String[] result){
+			//update the database
+			SQLiteDatabase db = dbhelper.getWritableDatabase();
+			
+			for(int i=0; i<result.length; i++){
+				//set up a map for content values
+				ContentValues values = new ContentValues();
+				values.put("status", result[i]);
+				String[] where = {theURLS[i].toString()};
+				db.update("downloads", values, "url=?", where);
+				
+			}
+			
+			//close the database
+			db.close();
+			
+			//hide the progress bar
+			pdialog.hide();
+			
+			//rebuild the UI for this object
+			loadLinks();
+		}
+	}
 }
---
+++ src/com/funkymonkeysoftware/adm/download/HTTPChecker.java
@@ -0,0 +1,54 @@
+package com.funkymonkeysoftware.adm.download;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+/**
+ * HTTP checker - checks http status code of given url
+ * 
+ * @author James Ravenscroft
+ *
+ */
+public class HTTPChecker implements LinkChecker {
+
+	@Override
+	public String getMatchPattern() {
+		return "http(s?)\\://\\S+";
+	}
+
+	/**
+	 * HTTP checker is a base module and therefore has low priority
+	 */
+	@Override
+	public Integer getPriority() {
+		return 0;
+	}
+
+	/**
+	 * This is where the checker actually does its magic!
+	 */
+	@Override
+	public String checkURL(URL theUrl) throws IOException {
+		//build a HTTP getter for the 
+		HttpClient client = new DefaultHttpClient();
+		//build a HEAD request for the url
+		HttpHead h = new HttpHead(theUrl.toString());
+		//get head
+		HttpResponse resp = client.execute(h);
+		
+		if(resp.getStatusLine().getStatusCode() == 200 ){
+			return "online";
+		}else{
+			return "offline";
+		}
+	}
+
+}
